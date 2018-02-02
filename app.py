eb720642a272c847c9e1ff2ec478f8e16f5825f6
Nikish Parikh <nikish.parikh@v2solutions.com>
2018-02-02 18:59:37 +0530

    default temp unit removed

--- app.py
+++ app.py
@@ -69,6 +69,9 @@ def makeYqlQuery(req):
     city = parameters.get("geo-city")
     tempUnit = parameters.get("V2Temperature")
     
+    if tempUnit is None:
+        return None
+
     if tempUnit is None or tempUnit.upper().startswith('F'):
        tempUnit = 'F'
     else:
